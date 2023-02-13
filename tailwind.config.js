d24c927a7daa6371c92dc360a0c758040b00ea3f
Hugo Tiburcio OSF <hugotiburcio@outlook.com>
2023-02-13 17:57:44 -0300

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      purple: "#755CDE",
+      orange: "#F6A560",
+      salmon: "#F39E9E",
+      red: "#EB7565",
+      green: "#61C4B7",
+      brown: "#552049",
+      black: "#030303",
+      gray: "#7A746E",
+      white: "#FFF7F0",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
